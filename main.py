637bc5706254814623386d8f93b5be222046edea
2084566953 <73601630+2084566953@users.noreply.github.com>
2022-08-24 19:52:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_weather():
   print(res)
   weather = res['data']['list'][0]
   print(weather)
-  return weather['weather'], weather['high'],weather['province'],weather[' ']
+  return weather['weather'], weather['high'],weather['province'],weather['city']
 
 # def get_count():
 #   print(today)
